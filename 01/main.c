747d036fe4e88ece65d0db426b19a8ebd46136cc
Jong-Chan Kim <jongchank@gmail.com>
2023-09-12 00:11:51 +0900

    Add error numbers
    - Returning the error number vs. using a separate variable (eno)
    - Manage error numbers using enum

--- 01/main.c
+++ 01/main.c
@@ -2,8 +2,18 @@
 #include <string.h>
 #include <assert.h>
 
+enum {
+    E_OK,           /* 0 */
+    E_NULL_MSG,     /* 1 */
+    E_ZERO_N,       /* 2 */
+    E_MAX_LEN_MSG,  /* 3 */
+    E_MAX_N         /* 4 */
+};
+
 #define MAX_LEN_MSG 30
 #define MAX_N 10
+
+int eno;
 
 int show(const char *msg, int n);
 
@@ -19,9 +29,9 @@ int main(void)
     if (n > MAX_N) {
         fprintf(stderr, "Above limit (%d)\n", n);
     }
-    ret = show(s, 5);
+    ret = show(s, n);
     if (ret < 0) {
-        fprintf(stderr, "show() failed. ret = %d\n", ret);
+        fprintf(stderr, "show() failed. ret = %d\n", eno);
     }
     return 0;
 }
@@ -33,17 +43,22 @@ int show(const char *msg, int n)
     assert(strlen(msg) != 0);
     assert(n != 0);
 
+    eno = E_OK;
     if (msg == NULL) {
+        eno = E_NULL_MSG;
         return -1;
     }
     if (n < 0) {
-        return -2;
+        eno = E_ZERO_N;
+        return -1;
     }
     if (strlen(msg) > MAX_LEN_MSG) {
-        return -3;
+        eno = E_MAX_LEN_MSG;
+        return -1;
     }
     if (n > MAX_N) {
-        return -4;
+        eno = E_MAX_N;
+        return -1;
     }
 
     for (i = 0; i < n; i++) {
