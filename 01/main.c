22fa697a70064d778cd79014410ec11aec9bf9c8
Jong-Chan Kim <jongchank@gmail.com>
2023-09-11 23:09:47 +0900

    Add basic error handling
    - See the importance of input validations
    - Use the function return value to let the caller know the error source

--- 01/main.c
+++ 01/main.c
@@ -4,13 +4,25 @@ int show(const char *msg, int n);
 
 int main(void)
 {
-    show("Hello\n", 5);
+    int ret;
+
+    ret = show("Hello\n", 5);
+    if (ret < 0) {
+        fprintf(stderr, "show() failed. ret = %d\n", ret);
+    }
     return 0;
 }
 
 int show(const char *msg, int n)
 {
     int i;
+
+    if (msg == NULL) {
+        return -1;
+    }
+    if (n < 0) {
+        return -2;
+    }
 
     for (i = 0; i < n; i++) {
         printf("%s", msg);
