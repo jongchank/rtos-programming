0e909e226ab456debd4ecde612d84311a4da240e
Jong-Chan Kim <jongchank@gmail.com>
2023-09-11 22:49:08 +0900

    Add show()
    - Tell the difference of show()'s declaration and definition
    - Test the effect of const when passing a pointer to a function

--- 01/main.c
+++ 01/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+int show(const char *msg, int n);
+
 int main(void)
 {
-    printf("Hello World\n");
+    show("Hello\n", 5);
+    return 0;
+}
+
+int show(const char *msg, int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++) {
+        printf("%s", msg);
+    }
     return 0;
 }
